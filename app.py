09f30aebadc5411023819715136f8f0f250ca84e
Vishal N <dev@vishalnandagopal.com>
2024-10-06 02:42:18 +0530

    github action next

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from sgb_fair_value_calculator import calculate_sgb_xirr, get_price_of_gold, get_sgbs
 
-_ = get_sgbs()
+all_sgbs = get_sgbs()
 
 current_gold_price = get_price_of_gold()
+max_sgb = all_sgbs[0]
 
-for sgb in _:
+for sgb in all_sgbs:
     sgb.xirr = calculate_sgb_xirr(sgb, current_gold_price)
+    if sgb.xirr > max_sgb.xirr:
+        max_sgb = sgb
 
-print(max(map(lambda x: x.xirr, _)))
+print(f"{max_sgb} will give you {max_sgb.xirr}% if gold price stays the same")
